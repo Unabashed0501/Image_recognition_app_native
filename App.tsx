d1315770048c8741a5399e09e0933386cc491ed7
oscarfu_0501 <59132665+Unabashed0501@users.noreply.github.com>
2024-05-01 16:25:31 +0800

    :tada: start react-native project

---
+++ App.tsx
@@ -0,0 +1,57 @@
+import { StatusBar } from "./node_modules/expo-status-bar/src/StatusBar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useRouter, Link } from "expo-router";
+import { NavigationContainer } from "@react-navigation/native";
+import Hello from "./components/hello";
+import WebSocketDemo from "./websocketDemo";
+import ClosetStack from "./pages/closet";
+import { MyHomeStack, HomeScreen, ProfileScreen } from "./pages/myHome";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import One from "./pages/one";
+
+export default function App() {
+  // const router = useRouter();
+
+  const Tab = createBottomTabNavigator();
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="HOME_STACK">
+        <Tab.Screen
+          name="HOME_STACK"
+          component={MyHomeStack}
+          options={{
+            title: "Home",
+            headerShown: false,
+          }}
+        />
+        <Tab.Screen name="Closet" component={ClosetStack} />
+        {/* <Tab.Screen name="Profile" component={ProfileScreen} /> */}
+        <Tab.Screen name="Hello" component={Hello} />
+        <Tab.Screen name="WebSocket" component={WebSocketDemo} /> 
+      </Tab.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    // {/* <Button onPress={() => router.push('/pages/closet')} title="Open Page1" /> */}
+    // <Link href="/pages/closet" asChild>
+    //   <Button title="Open Page1" />
+    // </Link> 
+    // {/* // <NavigationContainer>
+    // //   <Stack.Navigator initialRouteName="Home">
+    // //     <Stack.Screen name="Home" component={Test} />
+    // //     <Stack.Screen name="One" component={One} />
+    // //   </Stack.Navigator>
+    // // </NavigationContainer> */}
+    // </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
