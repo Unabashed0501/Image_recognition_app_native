3a7e8dba3095f0831d1f0066055a65be4eafe6ae
oscarfu_0501 <59132665+Unabashed0501@users.noreply.github.com>
2024-05-03 00:08:27 +0800

    👽️ Introduce weather api (#5)
    * 🙈 Update .gitignore to ignore .env files
    
    * 🙈 Ignore /.vscode/
    
    * 👽️ Introduce weather api

---
+++ api/apiOpenForecast.tsx
@@ -0,0 +1,39 @@
+import { Alert } from "react-native";
+// import dotenv from 'dotenv'
+// dotenv.config({ path: '../backend/.env' })
+
+// const appid = process.env.OPENWEATHER_API_KEY;
+const appid = "4cc078ae2b1c62edfc8f849122161e4a";
+
+async function fetchForecast(
+  latitude: number,
+  longitude: number,
+) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${appid}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch weather data");
+    }
+
+    const data = await response.json();
+    // console.log(data);
+
+    return data;
+  } catch (error) {
+    Alert.alert(
+      "Error",
+      "Error while fetching weather data: " + (error as Error).message
+    );
+    return null;
+  }
+}
+
+export default fetchForecast;
