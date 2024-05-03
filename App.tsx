4248807e07d5ac5034bfaa97e5ae42e6ca3af4b8
oscarfu_0501 <59132665+Unabashed0501@users.noreply.github.com>
2024-05-04 05:12:02 +0800

    ⬆️ Upgrade dependencies and App.tsx

--- App.tsx
+++ App.tsx
@@ -2,13 +2,15 @@ import { StatusBar } from "./node_modules/expo-status-bar/src/StatusBar";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { useRouter, Link } from "expo-router";
 import { NavigationContainer } from "@react-navigation/native";
-import Hello from "./components/hello";
+// import Hello from "./components/hello";
 import WebSocketDemo from "./websocketDemo";
 import ClosetStack from "./pages/closet";
 import { MyHomeStack, HomeScreen, ProfileScreen } from "./pages/myHome";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
-import One from "./pages/one";
+import HistoryPage from "./pages/historyPage";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 export default function App() {
   // const router = useRouter();
@@ -16,27 +18,43 @@ export default function App() {
   const Tab = createBottomTabNavigator();
   const Stack = createStackNavigator();
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="HOME_STACK">
-        <Tab.Screen
-          name="HOME_STACK"
-          component={MyHomeStack}
-          options={{
-            title: "Home",
-            headerShown: false,
-          }}
-        />
-        <Tab.Screen name="Closet" component={ClosetStack} />
-        {/* <Tab.Screen name="Profile" component={ProfileScreen} /> */}
-        <Tab.Screen name="Hello" component={Hello} />
-        <Tab.Screen name="WebSocket" component={WebSocketDemo} /> 
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="HOME_STACK">
+          <Tab.Screen
+            name="HOME_STACK"
+            component={MyHomeStack}
+            options={{
+              title: "Home",
+              headerShown: false,
+            }}
+          />
+          <Tab.Screen
+            name="Closet"
+            component={ClosetStack}
+            options={{
+              title: "Closet",
+              headerShown: false,
+            }}
+          />
+          {/* <Tab.Screen name="Profile" component={ProfileScreen} /> */}
+          <Tab.Screen
+            name="History"
+            component={HistoryPage}
+            options={{
+              title: "History",
+              headerShown: false,
+            }}
+          />
+          <Tab.Screen name="WebSocket" component={WebSocketDemo} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
     // <View style={styles.container}>
     // {/* <Button onPress={() => router.push('/pages/closet')} title="Open Page1" /> */}
     // <Link href="/pages/closet" asChild>
     //   <Button title="Open Page1" />
-    // </Link> 
+    // </Link>
     // {/* // <NavigationContainer>
     // //   <Stack.Navigator initialRouteName="Home">
     // //     <Stack.Screen name="Home" component={Test} />
