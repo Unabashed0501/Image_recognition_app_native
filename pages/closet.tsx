1411c87736d67cd2ba4251ae54f9f2e6250b7359
oscarfu_0501 <59132665+Unabashed0501@users.noreply.github.com>
2024-05-04 04:40:09 +0800

    🚑️ Hotfix pages

--- pages/closet.tsx
+++ pages/closet.tsx
@@ -2,75 +2,123 @@ import React, { useEffect, useState } from "react";
 import {
   FlatList,
   Image,
-  ScrollView,
   StyleSheet,
   Text,
   TextInput,
   View,
 } from "react-native";
-// import { NavigationContainer, RouteProp } from "@react-navigation/native";
-import { NavigationProp } from "@react-navigation/native";
-import useWebSocket, { ReadyState } from "react-native-use-websocket";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Catagory from "../components/catagory";
 import MyClothesCard from "../components/myClothesCard";
 // import Header from "../components/header";
-// import data from "../data/data.json";
-import { useNavigation } from "expo-router";
+import { queryAllEmbeddings } from "../api/apiClient"; // Import the queryAllEmbeddings function
 import { useFonts } from "@expo-google-fonts/poppins";
 import {
   StackNavigationProp,
   createStackNavigator,
 } from "@react-navigation/stack";
+import ActiveTabs from "../components/activeTabs";
+import { COLORS, SIZES } from "../styles/constants";
 import ClosetDetails from "./closetDetails";
+import { addToFavorites, removeFromFavorites } from "../redux/actions";
+import { connect, useDispatch, useSelector } from "react-redux";
+import { FavoritesState, RootState } from "../redux/reducers";
+import FavoriteItemsPage from "./favoriteItemsPage";
+
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 interface Product {
-  id: number;
+  id: string;
   isFavorite: boolean;
-}
-
-interface ProductDetailsParams {
-  item: Product;
+  imageUrl: string;
+  type: string;
+  title: string;
 }
 
 const ClosetStack = () => {
-  // const router = useRouter();
   const Stack = createStackNavigator();
 
   return (
     <Stack.Navigator initialRouteName="Closet">
       <Stack.Screen name="Closet" component={Closet} />
       <Stack.Screen name="ClosetDetails" component={ClosetDetails} />
+      <Stack.Screen name="Favorites" component={FavoriteItemsPage} />
+      {/* Add more screens as needed */}
     </Stack.Navigator>
   );
 };
 
-type ClosetDetailsProps = {
-  navigation: StackNavigationProp<RootStackParamList, "ClosetDetails">;
-};
+const Closet: React.FC<{ navigation: StackNavigationProp<any> }> = ({
+  navigation,
+}) => {
+  const [selectedCategory, setSelectedCategory] = useState("Shirts");
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [fontsLoaded, fontError] = useFonts({
+    "Poppins-Regular": require("../assets/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("../assets/Poppins-Medium.ttf"),
+    "Poppins-Bold": require("../assets/Poppins-Bold.ttf"),
+  });
+  const dispatch = useDispatch();
+  const favoriteItems = useSelector(
+    (state: FavoritesState) => state.favoriteItems
+  );
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return <Text>Qualifications</Text>;
 
-type RootStackParamList = {
-  ClosetDetails: { isFavorite: boolean };
-};
+      case "About":
+        return <Text>About</Text>;
 
-const Closet: React.FC<ClosetDetailsProps> = ({ navigation }) => {
-  const [selectedCatagory, setSelectedCatagory] = useState("Shirts");
-  const [products, setProducts] = useState(data.products);
-  // const navigation = useNavigation();
+      case "Responsibilities":
+        return <Text>Responsibility</Text>;
 
-  const handleClothesClick = () => {
-    navigation.navigate("ClosetDetails", { isFavorite: true });
+      default:
+        return null;
+    }
   };
 
-  const toggleFavorite = (
-    item: Product,
-    products: Product[],
-    setProducts: React.Dispatch<React.SetStateAction<Product[]>>
-  ) => {
-    setProducts(
-      products.map((prod) => {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const allEmbeddings = await queryAllEmbeddings();
+        console.log(allEmbeddings);
+        // Transform the data into the required format (id, isFavorite, imageUrl, type)
+        const transformedProducts: Product[] = allEmbeddings.map(
+          (embedding: any) => ({
+            id: embedding.metadata.id,
+            isFavorite: false, // You may set this to true based on some logic
+            imageUrl: embedding.metadata.imageUrl,
+            type: embedding.metadata.type,
+            title: embedding.metadata.title,
+          })
+        );
+        setProducts(transformedProducts);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  useEffect(() => {
+    console.log("Favorite Items:", favoriteItems);
+  }, [favoriteItems]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  const handleClothesClick = () => {
+    // navigation.navigate("ClosetDetails", { isFavorite: true });
+    navigation.navigate("Favorites");
+  };
+
+  const toggleFavorite = (item: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((prod) => {
         if (prod.id === item.id) {
-          console.log("prod: ", prod);
           return {
             ...prod,
             isFavorite: !prod.isFavorite,
@@ -79,62 +127,38 @@ const Closet: React.FC<ClosetDetailsProps> = ({ navigation }) => {
         return prod;
       })
     );
+    if (item.isFavorite) {
+      dispatch(removeFromFavorites(item.id) as any); // Dispatch the removeFromFavorites action
+    } else {
+      dispatch(addToFavorites(item) as any); // Dispatch the addToFavorites action
+    }
   };
-  const [images, setImages] = useState<string[]>([]);
-  // const { sendMessage, lastMessage } = useWebSocket("ws://localhost:8080", {
-  //   onOpen: () => console.log("opened"),
-  //   onClose: () => console.log("closed"),
-  //   onMessage: (message) => {
-  //     console.log("use effect message", message);
-  //     setImages((images) => [...images, message.data]);
-  //   },
-  //   shouldReconnect: (closeEvent) => true,
-  // });
-
-  // useEffect(() => {
-  //   sendMessage("getEmbedding");
-  // }, []);
-
-  const [fontsLoaded, fontError] = useFonts({
-    "Poppins-Regular": require("../assets/Poppins-Regular.ttf"),
-    "Poppins-Medium": require("../assets/Poppins-Medium.ttf"),
-    "Poppins-Bold": require("../assets/Poppins-Bold.ttf"),
-  });
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
 
   return (
-    <SafeAreaView>
-      <FlatList
-        data={["Shirts", "Pants", "Socks", "Shoes", "Accessories"]}
-        renderItem={({ item }) => (
-          <Catagory
-            item={item}
-            selectedCatagory={selectedCatagory}
-            setSelectedCatagory={setSelectedCatagory}
-          />
-        )}
-        keyExtractor={(item: any) => item.toString()}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-      />
+    <View style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <View style={{ padding: SIZES.medium, paddingBottom: 10 }}>
+        <ActiveTabs
+          tabs={tabs}
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+        />
+
+        {displayTabContent()}
+      </View>
       <FlatList
         ListHeaderComponent={
           <>
-            <>
-              <Header isCart={false} />
-              <View>
-                <Text style={styles.headingText}>Match Your Style</Text>
-                <View style={styles.inputContainer}>
-                  <Image
-                    source={require("../assets/search.png")}
-                    style={styles.searchIcon}
-                  />
-                  <TextInput placeholder="Search" style={styles.textInput} />
-                </View>
+            {/* <Header isCart={false} /> */}
+            <View>
+              <Text style={styles.headingText}>Match Your Style</Text>
+              <View style={styles.inputContainer}>
+                <Image
+                  source={require("../assets/images/search.png")}
+                  style={styles.searchIcon}
+                />
+                <TextInput placeholder="Search" style={styles.textInput} />
               </View>
-            </>
+            </View>
           </>
         }
         data={products}
@@ -143,20 +167,32 @@ const Closet: React.FC<ClosetDetailsProps> = ({ navigation }) => {
           <MyClothesCard
             item={item}
             handleClothesClick={handleClothesClick}
-            toggleFavorite={toggleFavorite}
+            toggleFavorite={() => toggleFavorite(item)}
           />
         )}
         showsVerticalScrollIndicator={false}
       />
-    </SafeAreaView>
+    </View>
   );
 };
+
+const mapStateToProps = (state: RootState) => ({
+  products: state.products,
+});
+
+const mapDispatchToProps = {
+  addToFavorites,
+  removeFromFavorites, // Add removeFromFavorites to mapDispatchToProps
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClosetStack);
+
 const styles = StyleSheet.create({
   headingText: {
     fontSize: 28,
     color: "#000000",
     marginVertical: 20,
-    fontFamily: "Poppins-Regular",
+    padding: 10,
   },
   inputContainer: {
     width: "100%",
@@ -173,7 +209,5 @@ const styles = StyleSheet.create({
   },
   textInput: {
     fontSize: 18,
-    fontFamily: "Poppins-Regular",
   },
 });
-export default ClosetStack;
--- pages/myHome.tsx
+++ pages/myHome.tsx
@@ -1,38 +1,74 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Button, SafeAreaView } from "react-native";
-import { useRouter, Link } from "expo-router";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { NavigationContainer, RouteProp } from "@react-navigation/native";
-import { useNavigation } from "@react-navigation/native";
+import useLocation from "../geocoder/useLocation";
+import fetchWeather from "../api/apiOpenWeather";
+import WeatherScreen from "../components/weather";
 
 const MyHomeStack = () => {
-  // const router = useRouter();
   const Stack = createStackNavigator();
+  const cureentlocation = useLocation();
+
+  useEffect(
+    () => console.log("current location", cureentlocation),
+    [cureentlocation]
+  );
 
   return (
     <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Weather" component={WeatherScreen} />
     </Stack.Navigator>
   );
 };
 
 type RootStackParamList = {
   Profile: { name: string };
+  Weather: undefined;
 };
 
 type HomeScreenProps = {
   navigation: StackNavigationProp<RootStackParamList, "Profile">;
+  cureentlocation: { latitude: number; longitude: number } | null;
 };
 
-const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
+const HomeScreen: React.FC<HomeScreenProps> = ({
+  navigation,
+  cureentlocation,
+}) => {
+  const handleFetchWeather = async () => {
+    const latitude = cureentlocation?.latitude;
+    const longitude = cureentlocation?.longitude;
+    if (latitude !== undefined && longitude !== undefined) {
+      console.log("Fetching weather");
+      const responseData = await fetchWeather(latitude, longitude);
+      console.log(responseData);
+    } else {
+      console.log("Location not available");
+    }
+  };
+
   return (
     <SafeAreaView>
       <Button
         title="Go to Oscar's profile"
         onPress={() => navigation.navigate("Profile", { name: "Oscar" })}
       />
+      <Button
+        title="Fetch weather in your area"
+        onPress={() => handleFetchWeather()}
+      />
+      <Button
+        title="View Weather"
+        onPress={() => navigation.navigate("Weather")} // Navigate to the Weather page
+      />
+      {/* <Button
+      title="Save Embeddings"
+      onPress={() => handleSaveEmbeddings()}
+    /> */}
     </SafeAreaView>
   );
 };
